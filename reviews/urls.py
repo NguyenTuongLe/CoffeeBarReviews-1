5292edf799faf295e437a56a81030b0eabed0e31
tiennl <nlthtien@gmail.com>
2021-05-03 10:42:46 +0700

    namespace url names

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = 'reviews'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:coffeeBar_id>/', views.detail, name='detail'),
