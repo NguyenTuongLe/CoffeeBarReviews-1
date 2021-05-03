64aebedc361538ecb565520ebb7b0f7bb584a168
tiennl <nlthtien@gmail.com>
2021-05-03 09:55:10 +0700

    render

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:coffeeBar_id>/', views.detail, name='detail'),
+    path('<int:coffeeBar_id>/results/', views.results, name='results'),
+    path('<int:coffeeBar_id>/vote/', views.vote, name='vote'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,21 @@
 from django.http import HttpResponse
+from django.template import loader
 
+from django.shortcuts import render
+
+from .models import User, CoffeeBar, Menu, Review
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the reviews index.")
+    latest_coffeeBar_list = CoffeeBar.objects.order_by('-avg_vote')[:10]
+    context = {'latest_coffeeBar_list': latest_coffeeBar_list}
+    return render(request, 'reviews/index.html', context)
+
+def detail(request, coffeeBar_id):
+    return HttpResponse("Quán %s." % coffeeBar_id)
+
+def results(request, coffeeBar_id):
+    response = "Review quán %s."
+    return HttpResponse(response % coffeeBar_id)
+
+def vote(request, coffeeBar_id):
+    return HttpResponse("Bạn đang đánh giá quán %s." % coffeeBar_id)
