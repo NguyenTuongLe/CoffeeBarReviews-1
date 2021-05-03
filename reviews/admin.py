2a105d31b8b8eb2400b619ae4056f54910ac8c3d
tiennl <nlthtien@gmail.com>
2021-05-03 16:34:34 +0700

    admin site

--- reviews/admin.py
+++ reviews/admin.py
@@ -6,27 +6,65 @@ class MenuInline(admin.TabularInline):
     model = Menu
     extra = 0
 
+
 class ReviewInline(admin.TabularInline):
     model = Review
     extra = 0
 
+
 class CoffeeBarAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['name']}),
+        (None, {'fields': ['name']}),
         (None, {'fields': ['avg_vote']}),
         (None, {'fields': ['address']}),
         (None, {'fields': ['description']}),
         (None, {'fields': ['created_at'], 'classes': ['collapse']}),
     ]
     inlines = [MenuInline, ReviewInline]
-    list_display = ('name', 'created_at')
+    list_display = ('name', 'avg_vote')
     search_fields = ['name']
+
+
+class MenuAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['coffeeBar']}),
+        (None, {'fields': ['name']}),
+        (None, {'fields': ['price']}),
+        (None, {'fields': ['description'], 'classes': ['collapse']}),
+    ]
+    list_display = ('coffeeBar', 'name', 'price')
+    search_fields = ['name']
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['coffeeBar']}),
+        (None, {'fields': ['user']}),
+        (None, {'fields': ['vote']}),
+        (None, {'fields': ['description']}),
+        (None, {'fields': ['created_at'], 'classes': ['collapse']}),
+    ]
+    list_display = ('coffeeBar', 'vote', 'description')
+    search_fields = ['coffeeBar']
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['firstName']}),
+        (None, {'fields': ['lastName']}),
+        (None, {'fields': ['email']}),
+        (None, {'fields': ['phone']}),
+        (None, {'fields': ['password']}),
+        (None, {'fields': ['role'], 'classes': ['collapse']}),
+    ]
+    list_display = ('firstName', 'lastName', 'email', 'phone')
+    search_fields = ['firstName']
 
 
 admin.site.register(CoffeeBar, CoffeeBarAdmin)
 
-admin.site.register(User)
+admin.site.register(Menu, MenuAdmin)
 
-admin.site.register(Menu)
+admin.site.register(Review, ReviewAdmin)
 
-admin.site.register(Review)
+admin.site.register(User, UserAdmin)
