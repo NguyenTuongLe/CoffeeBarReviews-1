a56f2c13103ac83e4dfb3879c74961f8c6de5792
NguyenTuongLe <tuongle200220@gmail.com>
2021-05-04 18:40:02 +0700

    view

---
+++ reviews/static/reviews/review.js
@@ -0,0 +1,2 @@
+//var myPieChart = new Chart(document.getElementById("canvas").getContext("2d")).Pie(PieChart);
+//var myPieChart = new Chart(document.getElementById("canvas").getContext("2d")).Pie(PieChart);
