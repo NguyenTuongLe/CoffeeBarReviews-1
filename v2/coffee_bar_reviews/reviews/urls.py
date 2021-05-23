383c9ff49c811ab6200d2d0ad5d887ea4f03f3f9
Toan Nguyen Dinh <toan@Toans-MacBook-Pro.local>
2021-05-23 11:45:24 +0700

    login and register

--- v2/coffee_bar_reviews/reviews/urls.py
+++ v2/coffee_bar_reviews/reviews/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import coffee_bars_list_view
+from .views import coffee_bars_list_view, register_view, login_view
 
 app_name = 'reviews'
 urlpatterns = [
-    path('', coffee_bars_list_view, name='coffee-bar-list-view')
+    path('', coffee_bars_list_view, name='home'),
+    path('register', register_view, name='register-view'),
+    path('login', login_view, name='login-view'),
 ]
--- v2/coffee_bar_reviews/reviews/views.py
+++ v2/coffee_bar_reviews/reviews/views.py
@@ -1,11 +1,42 @@
-from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
 from .models import CoffeeBar
+from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
 
 
-# Create your views here.
+# coffee bars list view
 def coffee_bars_list_view(request):
     coffee_bars = CoffeeBar.objects.order_by("-created_at").all()
     context = {
         "list": coffee_bars
     }
     return render(request, "reviews/coffee_bars.html", context)
+
+
+# register view
+def register_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/reviews')
+    else:
+        form = SignUpForm()
+    return render(request, 'reviews/register.html', {'form': form})
+
+
+# Login view
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.user_cache)
+            return redirect('/reviews')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'reviews/login.html', {'form': form})
